ca8cbaf5d11a4f332edff16ef1dda423b8cd603a
tamamunniam2001 <tamamunniam3@gmail.com>
2026-09-16 15:07:03 +0700

    Revert "rebrand: Bumi Kopi → Humbl"
    This reverts commit 5f53096b168017dd0c5b654a78d63b6eb10f651b.

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Humbl - Admin',
-  description: 'Backoffice Humbl',
+  title: 'Bumi Kopi - Admin',
+  description: 'Backoffice Bumi Kopi',
 }
 
 // Script ini dijalankan sebelum render untuk mencegah flash of wrong theme
